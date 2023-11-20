457c725039a7be655d71bf76109d8e0573dc04f1
Andre <andresanto.dev@gmail.com>
2023-11-20 12:34:36 +0000

    🚧 criado o form do frontend do criar veiculo

--- view/viewHome.php
+++ view/viewHome.php
@@ -10,6 +10,32 @@
     <p>cadastre aqui um novo veiculo</p>
     <form action="../controller/veiculoController.php" method="post">
 
+        <label for="tipo">Tipo do veículo:</label>
+        <input type="text" name="tipo" id="tipo" placeholder="digite o tipo do veículo" required maxlength="255">
+
+
+        <label for="marca">Marca do veículo:</label>
+        <input type="text" name="marca" id="marca" placeholder="digite a marca do veículo" required maxlength="255">
+
+
+        <label for="modelo">Modelo do veículo:</label>
+        <input type="text" name="modelo" id="modelo" placeholder="digite o modelo do veículo" required maxlength="255">
+       
+
+        <label for="cor">Cor do veículo:</label>
+        <input type="text" name="cor" id="cor" placeholder="digite a cor do veículo" required maxlength="255">
+      
+      
+        <label for="placa">Placa do veículo:</label>
+        <input type="text" name="placa" id="placa" placeholder="digite a placa do veículo" required maxlength="255">
+       
+       
+        <label for="ano">Ano do veículo:</label>
+        <input type="number" name="ano" id="ano" placeholder="digite o ano do veículo" required >
+
+
+        <input type="submit" value="Cadastrar">
+
     </form>
 </body>
 </html>
